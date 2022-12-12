db9938bfc7f4e6e7a1f7fbb14ea173bb555446ff
bibik_k <kostya.bibik02@mail.ru>
2022-12-12 15:57:09 +0300

    add btns on screens

--- Assets/Scripts/UI/Panels/Impl/WinPanelView.cs
+++ Assets/Scripts/UI/Panels/Impl/WinPanelView.cs
@@ -1,6 +1,28 @@
-﻿namespace UI.Panels.Impl
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI.Panels.Impl
 {
     public class WinPanelView : UiPanel
     {
+        [SerializeField] private Button restartBtn;
+        [SerializeField] private Button exitBtn;
+        
+        private void Start()
+        {
+            exitBtn.onClick.AddListener(delegate
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+            });
+            restartBtn.onClick.AddListener(delegate
+            {
+                SceneManager.LoadScene(0);
+            });
+        }
     }
 }
